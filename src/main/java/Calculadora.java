6994f574b7449d74c5ff2642dcc568dd1053500c
cleber da silva <cleber.supertux@gmail.com>
2023-12-22 13:48:37 -0300

    Assertion Equals

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -7,10 +7,6 @@ public class Calculadora {
 	
 	public static void main(String[] args) {
 		Calculadora calc = new Calculadora();
-		
-		System.out.println(calc.soma(2, 3) == 5);
-		System.out.println(calc.soma(4, 3) == 7);
-		System.out.println(calc.soma(5, 6) == 11);
 	}
 
 }
