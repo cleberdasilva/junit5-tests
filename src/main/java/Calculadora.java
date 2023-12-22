eaccb634d302a5eac091cbb648c5500bc6eab111
cleber da silva <cleber.supertux@gmail.com>
2023-12-22 10:17:05 -0300

    Initial tests

---
+++ src/main/java/Calculadora.java
@@ -0,0 +1,16 @@
+
+public class Calculadora {
+	
+	public int soma(int a, int b) {
+		return a + b;
+	}
+	
+	public static void main(String[] args) {
+		Calculadora calc = new Calculadora();
+		
+		System.out.println(calc.soma(2, 3) == 5);
+		System.out.println(calc.soma(4, 3) == 7);
+		System.out.println(calc.soma(5, 6) == 11);
+	}
+
+}
