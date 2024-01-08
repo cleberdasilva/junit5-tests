0bae7597237bfc63e495b5bf818254c08f0bade4
Cleber da Silva <cleber.supertux@gmail.com>
2024-01-08 11:27:16 -0300

    Usuario test parameterized

--- src/test/java/com/pingosystem/barriga/domain/UsuarioTest.java
+++ src/test/java/com/pingosystem/barriga/domain/UsuarioTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import com.pingosystem.barriga.domain.exceptions.ValidationException;
 
@@ -43,5 +45,19 @@ public class UsuarioTest {
 		ValidationException ex = Assertions.assertThrows(ValidationException.class, () ->
 		umUsuario().comSenha(null).agora());
 		assertEquals("Password is riquired", ex.getMessage());
+	}
+	
+	//with @ParameterizedTest all the tests above aren't necessary
+	
+	@ParameterizedTest(name = "[{index}] -{4}") //index and test name will appear at test execution
+	@CsvSource(value = {
+			"1, NULL, user@gmail.com, 12345678, Name is riquired",
+			"1, Nome Usuario, NULL, 12345678, Email is riquired",
+			"1, Nome Usuario, user@gmail.com, NULL, Password is riquired"
+	}, nullValues = "NULL")
+	public void deveValidarCamposObrigatorios(Long id, String nome, String email, String senha, String mensagem) {
+		ValidationException ex = Assertions.assertThrows(ValidationException.class, () ->
+		umUsuario().comId(id).comNome(nome).comEmail(email).comSenha(senha).agora());
+		assertEquals(mensagem, ex.getMessage());
 	}
 }
