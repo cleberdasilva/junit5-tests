94423756db5daa60f48a39a73dcc258a55c4c102
Cleber da Silva <cleber.supertux@gmail.com>
2024-01-12 09:20:50 -0300

    Starting with Mockito

--- src/test/java/com/pingosystem/barriga/service/UsuarioServiceTest.java
+++ src/test/java/com/pingosystem/barriga/service/UsuarioServiceTest.java
@@ -1,13 +1,25 @@
 package com.pingosystem.barriga.service;
 
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import com.pingosystem.barriga.domain.Usuario;
+import com.pingosystem.barriga.service.repositories.UsuarioRepository;
 
 public class UsuarioServiceTest {
 	private UsuarioService service;
 	
 	@Test
-	public void deveSalvarUsuarioComSucesso() {
+	public void deveRetornarUsuarioPorEmail() {
+		UsuarioRepository usarioRepository = Mockito.mock(UsuarioRepository.class);
+		service = new UsuarioService(usarioRepository);
 		
+		Optional<Usuario> user = service.getUserByEmail("mail@gmail.com");
+		Assertions.assertTrue(user.isEmpty());
+		//System.out.println(user);
 	}
 	
 	
